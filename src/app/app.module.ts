b0c098ccfe54b582b97977486405a97858903522
shivamkushwah-web <shivam.kushwah@joshtechnologygroup.com>
2026-07-21 19:04:59 +0530

    Added Base Setup (#1)
    * [SK_A2_01]: Added base
    
    * [SK_A2_01]: review fixes
    
    * [SK_A2_01]: fixed coderabbit review
    
    * [SK_A2_01]: fixed coderabbit review
    
    * [SK_A2_01]: extends pretter in the eslintconfig
    
    * [SK_A2_01]: resolved review comments
    
    * [SK_A2_01]: resolved review comments
    
    * [SK_A2_01]: update path aliases in tsconfig.json
    
    * [SK_A2_01]: fixed tsconfig
    
    * [SK_A2_01]: added nvmrc and added headline 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatCardModule,
+    MatListModule,
+    MatInputModule,
+    MatFormFieldModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
